98ae06e1204857aa4e8fa1d92fd8e3dbd2e3e599
Solomka <sol.yaremko@gmail.com>
2017-03-02 21:33:22 +0200

    main

--- src/MultiThreadsArraySort.java
+++ src/MultiThreadsArraySort.java
@@ -27,7 +27,7 @@ public class MultiThreadsArraySort {
 				"UTF-8"));
 
 		System.out.println("File size: " + br.readLine() + "\nFile lines: "
-				+ size); 
+				+ size);
 
 		for (int i = 0; i < size; i++) {
 			data[i] = br.readLine();
@@ -35,35 +35,31 @@ public class MultiThreadsArraySort {
 		return data;
 	}
 
+	/*
+	 * Threads num and data size config
+	 */
+
 	// 500 000 - max number of lines to read from file
 	private static final int LINES_NUM_TO_READ = 1000;
 	private static final int THREADS_NUM = 10;
-	private static double st, en;
 
-	public static void main(String[] args) throws IOException {
-		/*
-		 * final String[] input = { "mother", "father", "sister", "brother",
-		 * "another", "eclipse" }; System.out.println("Before Sorting - " +
-		 * Arrays.toString(input));
-		 * //BinaryInsertionSort.binaryInsertionSort(input);
-		 * RadixSort.radixSort(input); System.out.println("After Sorting - " +
-		 * Arrays.toString(input));
-		 */
+	private static ArrayList<String[]> insSortArr;
+	private static ArrayList<String[]> radixSortArr;
 
-		String[] data = readFileData("500.000.txt", LINES_NUM_TO_READ);
-
-		/*
-		 * data for Binary Insertion Sort
-		 */
+	/*
+	 * Split main array into threads subarrays
+	 */
+	private static ArrayList<String[]> splitArray(String[] data) {
 		ArrayList<String[]> arrays = new ArrayList<String[]>(THREADS_NUM);
-		int residue = LINES_NUM_TO_READ % THREADS_NUM;
-		int arrSize = (LINES_NUM_TO_READ - residue) / THREADS_NUM;
-		int counter = 0; // general counter, in the end must = SIZE.
+
+		int remainder = LINES_NUM_TO_READ % THREADS_NUM;
+		int arrSize = LINES_NUM_TO_READ / THREADS_NUM;
+		int counter = 0;
 
 		// splitting to N arrays
 		for (int i = 0; i < THREADS_NUM; i++) {
 			String[] temp;
-			if (residue > 0) {
+			if (remainder > 0) {
 				temp = new String[arrSize + 1];
 			} else {
 				temp = new String[arrSize];
@@ -72,122 +68,139 @@ public class MultiThreadsArraySort {
 				temp[j] = data[counter];
 				counter++;
 			}
-			if (residue > 0) {
+			if (remainder > 0) {
 				temp[temp.length - 1] = data[counter];
 				counter++;
-				residue--;
+				remainder--;
 			}
 			arrays.add(temp);
 		}
-
-		// data for Radix Sort
-		ArrayList<String[]> arrays1 = new ArrayList<String[]>(arrays); // fine
-																		// only
-																		// for
-																		// IMMUTABLE
-																		// objects
-
-		/*
-		 * runs BInaryInsertionSort in N threads and waits until all done
-		 */
-		st = System.nanoTime();
-		ExecutorService pool = Executors.newFixedThreadPool(THREADS_NUM);
-		List<Callable<Object>> tasks = new ArrayList<>();
-		try {
-			for (String[] element : arrays) {
-				tasks.add(new Callable<Object>() { // adds tasks
-					public Object call() throws Exception {
-						BinaryInsertionSort.binaryInsertionSort(element);
-						return element;
-					}
-				});
-			}
-			List<Future<Object>> invokeAll = pool.invokeAll(tasks); // runs pool
-																	// of
-																	// threads
-																	// and WAIT
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			pool.shutdown(); // the end
-		}
-
-		// merging
-		String insertionSortResult[] = ArraysMerger.mergeMultithread(arrays);
-
-		en = System.nanoTime();
-
-		// writing to file
-		try (FileWriter writer = new FileWriter(
-				"BinaryInsertionSortResult.txt", false)) {
-			for (String element : insertionSortResult) {
-				writer.write(element);
-				writer.append('\n');
-			}
-			writer.write("Sorted in: " + ((en - st) / 1000000000) + " seconds");
-			writer.append('\n');
-			writer.write("By: " + THREADS_NUM + " threads");
-			writer.flush();
-		} catch (IOException ex) {
-			System.out.println(ex.getMessage());
-		}
-		System.out.println("Binary Insertion Sort (" + THREADS_NUM
-				+ " Thread(s)) finished in: " + ((en - st) / 1000000000)
-				+ " seconds");
-
-		st = 0;
-		en = 0;
-
-		/*
-		 * runs RadixSort in N threads and waits until all done
-		 */
-		st = System.nanoTime();
-		ExecutorService pool1 = Executors.newFixedThreadPool(THREADS_NUM);
-		List<Callable<Object>> tasks1 = new ArrayList<>();
-		try {
-			for (String[] element : arrays1) {
-				tasks1.add(new Callable<Object>() { // adds tasks
-					public Object call() throws Exception {
-						RadixSort.radixSort(element);
-						return element;
-					}
-				});
-			}
-			List<Future<Object>> invokeAll1 = pool1.invokeAll(tasks1); // runs
-																		// pool
-																		// of
-																		// threads
-																		// and
-																		// WAIT
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		} finally {
-			pool1.shutdown(); // the end
-		}
-
-		// merging
-		String mergeSortResult[] = ArraysMerger.mergeMultithread(arrays1);
-
-		en = System.nanoTime();
-
-		// writing to file
-		try (FileWriter writer = new FileWriter("RadixSortResult.txt", false)) {
-			for (String element : mergeSortResult) {
-				writer.write(element);
-				writer.append('\n');
-			}
-			writer.write("Sorted in: " + ((en - st) / 1000000000) + " seconds");
-			writer.append('\n');
-			writer.write("By: " + THREADS_NUM + " threads");
-			writer.flush();
-		} catch (IOException ex) {
-			System.out.println(ex.getMessage());
-		}
-
-		System.out.println("Radix Sort (" + THREADS_NUM
-				+ " Thread(s)) finished in: " + ((en - st) / 1000000000)
-				+ " seconds");
+		return arrays;
 
 	}
 
+	/*
+	 * process multythreading array sorting
+	 */
+	private static void processMultyThreadsSorting(String sortingType) {
+
+		double st = 0;
+		double en = 0;
+
+		if (sortingType == "binary") {
+			st = System.nanoTime();
+			// create pool of threads
+			ExecutorService pool = Executors.newFixedThreadPool(THREADS_NUM);
+			List<Callable<Object>> tasks = new ArrayList<>();
+			try {
+				for (String[] subArray : insSortArr) {
+					tasks.add(new Callable<Object>() {
+						public Object call() throws Exception {
+							BinaryInsertionSort.binaryInsertionSort(subArray);
+							return subArray;
+						}
+					});
+				}
+				// run pool of threads
+				List<Future<Object>> invokeAll = pool.invokeAll(tasks);
+
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				pool.shutdown(); // the end
+			}
+
+			// merging
+			String binaryInsertionSortResult[] = ArraysMerger
+					.mergeMultithread(insSortArr);
+
+			en = System.nanoTime();
+			writeResult(st, en, binaryInsertionSortResult,
+					"BinaryInsertionSortResult.txt");
+		} else if (sortingType == "radix") {
+
+			st = System.nanoTime();
+			// create pool of threads
+			ExecutorService pool = Executors.newFixedThreadPool(THREADS_NUM);
+			List<Callable<Object>> tasks = new ArrayList<>();
+			try {
+				for (String[] subArray : radixSortArr) {
+					tasks.add(new Callable<Object>() {
+						public Object call() throws Exception {
+							BinaryInsertionSort.binaryInsertionSort(subArray);
+							return subArray;
+						}
+					});
+				}
+				// run pool of threads
+				List<Future<Object>> invokeAll = pool.invokeAll(tasks);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			} finally {
+				pool.shutdown(); // the end
+			}
+
+			// merging
+			String radixSortResult[] = ArraysMerger
+					.mergeMultithread(radixSortArr);
+
+			en = System.nanoTime();
+			writeResult(st, en, radixSortResult, "RadixSortResult.txt");
+
+		} else {
+			//
+		}
+
+	}
+
+	private static void writeResult(double st, double en, String[] sortedArray,
+			String resultFile) {
+
+		// writing to file
+		try (FileWriter writer = new FileWriter(resultFile, false)) {
+			for (String element : sortedArray) {
+				writer.write(element);
+				writer.append('\n');
+			}
+			writer.write("\nStatistics");
+			writer.write("\nSorted in: " + ((en - st) / 1000000000)
+					+ " seconds");
+			writer.append('\n');
+			writer.write("By: " + THREADS_NUM + " threads");
+			writer.flush();
+		} catch (IOException ex) {
+			System.out.println(ex.getMessage());
+		}
+		System.out.println(resultFile.substring(0, resultFile.indexOf('.'))
+				+ " " + THREADS_NUM + " Thread(s)\nFinished in: "
+				+ ((en - st) / 1000000000) + " seconds");
+	}
+
+	public static void main(String[] args) throws IOException {
+
+		String[] data = readFileData("500.000.txt", LINES_NUM_TO_READ);
+
+		/*
+		 * data for Binary Insertion Sort
+		 */
+
+		insSortArr = splitArray(data);
+
+		/*
+		 * data for Radix Sort
+		 */
+		radixSortArr = new ArrayList<String[]>(insSortArr);
+
+		/*
+		 * runs BinaryInsertionSort in N threads and write result in file
+		 */
+
+		processMultyThreadsSorting("binary");
+		/*
+		 * runs BinaryInsertionSort in N threads and write result in file
+		 */
+
+		processMultyThreadsSorting("radix");
+
+	}
 }
